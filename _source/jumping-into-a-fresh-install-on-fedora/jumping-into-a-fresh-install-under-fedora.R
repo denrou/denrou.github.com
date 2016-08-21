7f7afb6f87db64dbeb73fc5e0a1db7815d650bb0
denrou <deroussel@gmail.com>
2016-08-22 00:14:34 +0200

    New theme

--- _source/jumping-into-a-fresh-install-on-fedora/jumping-into-a-fresh-install-under-fedora.R
+++
@@ -1,3 +0,0 @@
-# This R file accomanies the .Rmd blog post
-# _source/jumping-into-a-fresh-install-on-fedora/2016-07-24-jumping-into-a-fresh-install-on-fedora.Rmd
-
---
+++ _source/jumping-into-a-fresh-install-with-fedora/jumping-into-a-fresh-install-with-fedora.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/jumping-into-a-fresh-install-with-fedora/2016-08-20-jumping-into-a-fresh-install-with-fedora.Rmd
+
